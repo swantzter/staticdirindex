42cfbd7c53fb6622c14c7b71f72de872864cba79
Svante Bengtson <svante.b37@gmail.com>
2020-12-06 17:23:46 +0100

    v1.8

--- staticdirindex/__init__.py
+++ staticdirindex/__init__.py
@@ -12,7 +12,7 @@ from mako.template import Template
 from mako.lookup import TemplateLookup
 from preview_generator.manager import PreviewManager
 
-__version__ = '1.7'
+__version__ = '1.8'
 
 EXCLUDED = ['index.html', 'header.html', '.git',
             '.gitignore', '.listignore', 'previews',
